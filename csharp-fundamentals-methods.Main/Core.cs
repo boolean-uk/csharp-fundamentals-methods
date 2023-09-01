de8e2383fd87493640495736f1a7ba652885a031
spiroskafiris <spiroskafiris@gmail.com>
2023-09-01 18:05:14 +0300

    Spiros Kafiris

--- csharp-fundamentals-methods.Main/Core.cs
+++ csharp-fundamentals-methods.Main/Core.cs
@@ -27,7 +27,7 @@ namespace csharp_fundamentals_methods.Main
         //TODO: 1. Create a method that accepts a name and returns a greeting      
         public string greet(string name)
         {
-            throw new NotImplementedException();
+            return($"Hello {name}!");
         }
 
         //TODO: 2. Increment a number
@@ -36,7 +36,7 @@ namespace csharp_fundamentals_methods.Main
          */
         public int increment(int number)
         {
-            throw new NotImplementedException();
+            return (number + 1);
         }
 
         //TODO: 3. Construct a friendly greeting
@@ -50,7 +50,7 @@ namespace csharp_fundamentals_methods.Main
          */
         public string happilyGreet(string name)
         {
-            throw new NotImplementedException();
+            return($"Hi, {name} :)");
         }
 
 
@@ -69,11 +69,12 @@ namespace csharp_fundamentals_methods.Main
 
         public int[] constructNumberArray(int lower, int upper)
         {
-
-            int[] resultArray = { };
-
+            int[] resultArray = new int[upper - lower + 1];
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = lower + i;
+            }
             return resultArray;
-
         }
 
 
@@ -92,7 +93,9 @@ namespace csharp_fundamentals_methods.Main
 
         public string shout(string phrase, int number)
         {
-            return $"";
+            StringBuilder shoutedString = new StringBuilder(phrase.ToUpper());
+            shoutedString.Append('!', number);
+            return shoutedString.ToString();
         }
 
 
