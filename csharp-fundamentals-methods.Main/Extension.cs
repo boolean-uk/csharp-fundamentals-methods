fdc47e8fbc23b1a07b9f3e1a90270a57bd2e4deb
Max Kroon <maxkroon.18@gmail.com>
2023-09-01 16:30:02 +0200

    core and extentions

--- csharp-fundamentals-methods.Main/Extension.cs
+++ csharp-fundamentals-methods.Main/Extension.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace csharp_fundamentals_methods.Main
 {
@@ -17,9 +18,32 @@ namespace csharp_fundamentals_methods.Main
         "The cake is still baking!" if there are any remaining minutes left,
         and "The timer finished ages ago!" if the remaining minutes is a negative number
      */
-        public double timerStatus(int v)
+
+        Baking status = new Baking() 
         {
-            throw new NotImplementedException();
+            isBaking = 5
+        };
+
+           
+        
+        public string timerStatus(int isBaking)
+        {
+            string result = string.Empty;
+            if (isBaking == 0)
+            {
+                result = "The cake is ready!";
+            }
+            if (isBaking < 0)
+            {
+                result = ("The timer finished ages ago!");
+            }
+            if (isBaking > 0)
+            {
+                result = ("The cake is still baking!");
+            }
+           
+            //throw new NotImplementedException();
+            return result;
         }
 
 
@@ -33,14 +57,27 @@ namespace csharp_fundamentals_methods.Main
             If a prep time of 0 is provided, the method should assume each ingredient takes 2 minutes to prepare.
          */
 
-        public double estimatePrepTime(string[] strings, int v)
+        public double estimatePrepTime(string[] ingredients, int timePerIngredient)
         {
-            throw new NotImplementedException();
+            if (timePerIngredient == 0)
+            {
+                timePerIngredient = 2;
+            }
+
+            int totalPrepTime = ingredients.Length * timePerIngredient;
+
+            return totalPrepTime;
+            //throw new NotImplementedException();
+
+
         }
 
 
 
-        //TODO: Extension 3: calculateGramsOfSugar that accepts two parameters 1 an array of ingredients that will always contain 3 ingredients AND  2 the number of layers the cake has. The cake will need 100g of sugar per layer, if that ingredient is present in the provided list of ingredients.             The method should return the number of grams of sugar needed to make the cake.
+        //TODO: Extension 3: calculateGramsOfSugar that accepts two parameters 1 an array
+        //of ingredients that will always contain 3 ingredients AND  2 the number of layers the cake has.  
+       // The cake will need 100g of sugar per layer, if that ingredient is present in the provided list of ingredients.
+       // The method should return the number of grams of sugar needed to make the cake.
         /*  3.
             Create a method named calculateGramsOfSugar that accepts two parameters:
             - an array of ingredients that will always contain 3 ingredients
@@ -49,9 +86,22 @@ namespace csharp_fundamentals_methods.Main
             The method should return the number of grams of sugar needed to make the cake.
          */
 
-        public double calculateGramsOfSugar(string[] strings, int v)
+        public double calculateGramsOfSugar(string[] ingredients, int numberOfLayers)
         {
-            throw new NotImplementedException();
+            string sugar = "sugar";
+
+            double totalSugar = 0;
+
+            foreach (string ingredient in ingredients)
+            {
+                if (ingredient == sugar)
+                {
+                    totalSugar += 100 * numberOfLayers;
+                }
+            }
+
+            return totalSugar;
+            //throw new NotImplementedException();
         }
 
 
---
+++ csharp-fundamentals-methods.Main/Numbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_fundamentals_methods.Main
+{
+    internal class Numbers
+    {
+        public int Increment { get; set; }
+        public int Construct { get; set; }
+        public int lower { get; set; }
+        public int upper { get; set; }
+
+    }
+}
