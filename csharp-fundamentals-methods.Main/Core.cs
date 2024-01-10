84ce3e71138fab844e4adf1195a1d48ee094f360
Najem Hamo <najem.hamo@gmail.com>
2024-01-10 09:15:32 +0100

    Core exercise is done.

--- csharp-fundamentals-methods.Main/Core.cs
+++ csharp-fundamentals-methods.Main/Core.cs
@@ -27,7 +27,7 @@ namespace csharp_fundamentals_methods.Main
         //TODO: 1. Create a method that accepts a name and returns a greeting      
         public string greet(string name)
         {
-            throw new NotImplementedException();
+            return $"Hello {name}!";
         }
 
         //TODO: 2. Increment a number
@@ -36,7 +36,8 @@ namespace csharp_fundamentals_methods.Main
          */
         public int increment(int number)
         {
-            throw new NotImplementedException();
+            number++;
+            return number;
         }
 
         //TODO: 3. Construct a friendly greeting
@@ -50,7 +51,7 @@ namespace csharp_fundamentals_methods.Main
          */
         public string happilyGreet(string name)
         {
-            throw new NotImplementedException();
+            return $"Hi, {name} :)";
         }
 
 
@@ -70,7 +71,11 @@ namespace csharp_fundamentals_methods.Main
         public int[] constructNumberArray(int lower, int upper)
         {
 
-            int[] resultArray = { };
+            int[] resultArray = new int[upper - lower + 1];
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = lower + i;
+            }
 
             return resultArray;
 
@@ -92,7 +97,12 @@ namespace csharp_fundamentals_methods.Main
 
         public string shout(string phrase, int number)
         {
-            return $"";
+            string result = phrase.ToUpper();
+            for (int i = 0; i < number; i++)
+            {
+                result += "!";
+            }
+            return $"{result}";
         }
 
 
