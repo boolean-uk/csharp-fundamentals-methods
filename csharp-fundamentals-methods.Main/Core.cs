8d2ea2d0cb0feb1983cbc4598054e14e2a6fa2a5
llllllll-l <vincent.werelius@gmail.com>
2024-01-10 09:05:00 +0100

    Core done and tested

--- csharp-fundamentals-methods.Main/Core.cs
+++ csharp-fundamentals-methods.Main/Core.cs
@@ -27,7 +27,7 @@ namespace csharp_fundamentals_methods.Main
         //TODO: 1. Create a method that accepts a name and returns a greeting      
         public string greet(string name)
         {
-            throw new NotImplementedException();
+            return "Hello " + name +"!";
         }
 
         //TODO: 2. Increment a number
@@ -36,7 +36,7 @@ namespace csharp_fundamentals_methods.Main
          */
         public int increment(int number)
         {
-            throw new NotImplementedException();
+            return ++number;
         }
 
         //TODO: 3. Construct a friendly greeting
@@ -50,7 +50,7 @@ namespace csharp_fundamentals_methods.Main
          */
         public string happilyGreet(string name)
         {
-            throw new NotImplementedException();
+            return "Hi, " + name + " :)";
         }
 
 
@@ -70,7 +70,12 @@ namespace csharp_fundamentals_methods.Main
         public int[] constructNumberArray(int lower, int upper)
         {
 
-            int[] resultArray = { };
+            int[] resultArray = new int[upper-lower+1];
+
+            for (int i = lower; i <= upper; i++)
+            {
+                resultArray[i-lower] = i;
+            }
 
             return resultArray;
 
