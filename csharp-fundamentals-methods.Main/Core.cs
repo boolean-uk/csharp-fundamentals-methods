238d661f2ac577351e96a2991ed479680875903e
Victor <victor.adamson@se.experis.com>
2024-01-10 09:25:51 +0100

    Solved core TODO

--- csharp-fundamentals-methods.Main/Core.cs
+++ csharp-fundamentals-methods.Main/Core.cs
@@ -27,7 +27,7 @@ namespace csharp_fundamentals_methods.Main
         //TODO: 1. Create a method that accepts a name and returns a greeting      
         public string greet(string name)
         {
-            throw new NotImplementedException();
+            return $"Hello {name}!";
         }
 
         //TODO: 2. Increment a number
@@ -36,7 +36,7 @@ namespace csharp_fundamentals_methods.Main
          */
         public int increment(int number)
         {
-            throw new NotImplementedException();
+            return number += 1;
         }
 
         //TODO: 3. Construct a friendly greeting
@@ -50,7 +50,7 @@ namespace csharp_fundamentals_methods.Main
          */
         public string happilyGreet(string name)
         {
-            throw new NotImplementedException();
+            return $"Hi, {name} :)";
         }
 
 
@@ -69,11 +69,13 @@ namespace csharp_fundamentals_methods.Main
 
         public int[] constructNumberArray(int lower, int upper)
         {
-
-            int[] resultArray = { };
-
+            List<int> result = new List<int>();
+            for(int i = lower; i <= upper; i++)
+            {
+                result.Add(i);
+            }
+            int[] resultArray = result.ToArray();
             return resultArray;
-
         }
 
 
@@ -92,7 +94,15 @@ namespace csharp_fundamentals_methods.Main
 
         public string shout(string phrase, int number)
         {
-            return $"";
+            phrase = phrase.ToUpper();
+            StringBuilder sb = new StringBuilder();
+            sb.Append(phrase);
+            for(int i = 0; i < number; i++)
+            {
+                sb.Append("!");
+            }
+                
+            return $"{sb}";
         }
 
 
